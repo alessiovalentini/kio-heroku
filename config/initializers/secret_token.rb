d19ae37b07582a79b48539e8fecfbfea324247d0
alessiovalentini <alessio.valentini@makepositive.com>
2013-01-08 11:28:09 +0000

    done: app initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kio::Application.config.secret_token = '27af47adcafb123e0ff94749905aad17de0427f82618903cb6e817c81e7b25f8a8f0e360aa9bce5f6032ec2a396bcf26c65219ddd2697becdf814650ef0b506b'
