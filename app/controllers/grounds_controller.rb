8f7d0b8b75a35568d0ffff2b7e99fb331ee1b125
ManuelOlvera <manuel.olvera@makepositive.com>
2013-01-09 16:40:10 +0000

    remove geokit

--- app/controllers/grounds_controller.rb
+++ app/controllers/grounds_controller.rb
@@ -2,9 +2,7 @@ class GroundsController < ApplicationController
   # GET /grounds
   # GET /grounds.json
   def index
-    # @grounds = Ground.all
-
-    Ground.find(:all, :origin =>[37.792,-122.393], :within=>10)
+    @grounds = Ground.all
 
     respond_to do |format|
       format.html # index.html.erb
