32a6a83a9df1f9966fbe408b0c989d51cb30b25a
alessiovalentini <alessio.valentini@makepositive.com>
2013-01-09 16:48:50 +0000

    done: updated ground model for geocoder

--- app/models/ground.rb
+++ app/models/ground.rb
@@ -1,3 +1,7 @@
 class Ground < ActiveRecord::Base
-  attr_accessible :groundName, :recordId
+  attr_accessible :groundName, :recordId, :latitude, :longitude
+
+  # get the grounds from salesforce | https://<instance_url>/services/apexrest/kio/v1.0/getGrounds
+  def self.get_grounds_from_salesforce
+  end
 end
