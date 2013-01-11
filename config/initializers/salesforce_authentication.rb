8fe27e2bf1ff39c43f3164f7ce37390f3c6cf090
alessiovalentini <alessio.valentini@makepositive.com>
2013-01-11 15:48:51 +0000

    done: set production environment with client id and secret and host

--- config/initializers/salesforce_authentication.rb
+++ config/initializers/salesforce_authentication.rb
@@ -5,7 +5,8 @@
 puts "> authentication with username and password"
 
 # generate client using heroku ENV vars and local export vars (see omniauth.rb) | :host when connecting to sandbox
-@@client = Databasedotcom::Client.new :client_id => "3MVG9_7ddP9KqTzcZNlY7gQI32.qDwVMzUggPsH_nI_f0CrSaCdOX6.48GNV5Kgt4Us12NrkcrNdg7p7M1RLv", :client_secret => "1074214033172658342", :host => "test.salesforce.com"
+# @@client = Databasedotcom::Client.new :client_id => "3MVG9_7ddP9KqTzcZNlY7gQI32.qDwVMzUggPsH_nI_f0CrSaCdOX6.48GNV5Kgt4Us12NrkcrNdg7p7M1RLv", :client_secret => "1074214033172658342", :host => "test.salesforce.com"
+@@client = Databasedotcom::Client.new :client_id => "3MVG99qusVZJwhsmKYfJHWTxa2xhAW.C0ON_RldSy3BK77TkjMDZhxe2k4yAW5JcZ5ckltwCx.dHRpytpf3b6", :client_secret => "5921374081795068997", :host => "login.salesforce.com"
 
 # authenticate directly with username and password+security token (not using oauth)
 @@client.authenticate :username => "muser@kio.com", :password => "5Zm%PnzcVqx8MHRzLrJFfTHY8kZ0ouTJQ6aYZ"
