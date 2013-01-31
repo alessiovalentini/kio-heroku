5d79c1e564b3f4b3bccea5248cc9d4fa5fd38f99
alessiovalentini <alessio.valentini@makepositive.com>
2013-01-31 16:51:22 +0000

    done: secured the app

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -21,21 +21,21 @@ class FeedbacksController < ApplicationController
     end
   end
 
-  # GET /feedbacks/new
-  # GET /feedbacks/new.json
-  def new
-    @feedback = Feedback.new
+  # # GET /feedbacks/new
+  # # GET /feedbacks/new.json
+  # def new
+  #   @feedback = Feedback.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @feedback }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @feedback }
+  #   end
+  # end
 
-  # GET /feedbacks/1/edit
-  def edit
-    @feedback = Feedback.find(params[:id])
-  end
+  # # GET /feedbacks/1/edit
+  # def edit
+  #   @feedback = Feedback.find(params[:id])
+  # end
 
   # POST /feedbacks
   # POST /feedbacks.json
@@ -55,29 +55,29 @@ class FeedbacksController < ApplicationController
 
   # PUT /feedbacks/1
   # PUT /feedbacks/1.json
-  def update
-    @feedback = Feedback.find(params[:id])
+  # def update
+  #   @feedback = Feedback.find(params[:id])
 
-    respond_to do |format|
-      if @feedback.update_attributes(params[:feedback])
-        format.html { redirect_to @feedback, notice: 'Feedback was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @feedback.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @feedback.update_attributes(params[:feedback])
+  #       format.html { redirect_to @feedback, notice: 'Feedback was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @feedback.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /feedbacks/1
   # DELETE /feedbacks/1.json
-  def destroy
-    @feedback = Feedback.find(params[:id])
-    @feedback.destroy
+  # def destroy
+  #   @feedback = Feedback.find(params[:id])
+  #   @feedback.destroy
 
-    respond_to do |format|
-      format.html { redirect_to feedbacks_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to feedbacks_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
--- app/controllers/grounds_controller.rb
+++ app/controllers/grounds_controller.rb
@@ -35,63 +35,63 @@ class GroundsController < ApplicationController
     end
   end
 
-  # GET /grounds/new
-  # GET /grounds/new.json
-  def new
-    @ground = Ground.new
+  # # GET /grounds/new
+  # # GET /grounds/new.json
+  # def new
+  #   @ground = Ground.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @ground }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @ground }
+  #   end
+  # end
 
-  # GET /grounds/1/edit
-  def edit
-    @ground = Ground.find(params[:id])
-  end
+  # # GET /grounds/1/edit
+  # def edit
+  #   @ground = Ground.find(params[:id])
+  # end
 
-  # POST /grounds
-  # POST /grounds.json
-  def create
-    @ground = Ground.new(params[:ground])
+  # # POST /grounds
+  # # POST /grounds.json
+  # def create
+  #   @ground = Ground.new(params[:ground])
 
-    respond_to do |format|
-      if @ground.save
-        format.html { redirect_to @ground, notice: 'Ground was successfully created.' }
-        format.json { render json: @ground, status: :created, location: @ground }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @ground.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @ground.save
+  #       format.html { redirect_to @ground, notice: 'Ground was successfully created.' }
+  #       format.json { render json: @ground, status: :created, location: @ground }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @ground.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PUT /grounds/1
-  # PUT /grounds/1.json
-  def update
-    @ground = Ground.find(params[:id])
+  # # PUT /grounds/1
+  # # PUT /grounds/1.json
+  # def update
+  #   @ground = Ground.find(params[:id])
 
-    respond_to do |format|
-      if @ground.update_attributes(params[:ground])
-        format.html { redirect_to @ground, notice: 'Ground was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @ground.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @ground.update_attributes(params[:ground])
+  #       format.html { redirect_to @ground, notice: 'Ground was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @ground.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /grounds/1
-  # DELETE /grounds/1.json
-  def destroy
-    @ground = Ground.find(params[:id])
-    @ground.destroy
+  # # DELETE /grounds/1
+  # # DELETE /grounds/1.json
+  # def destroy
+  #   @ground = Ground.find(params[:id])
+  #   @ground.destroy
 
-    respond_to do |format|
-      format.html { redirect_to grounds_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to grounds_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
